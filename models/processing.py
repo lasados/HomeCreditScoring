632f829bc6a1ff603e1ce2daa4343d977e8f1440
lasados <lasados96@gmail.com>
2020-10-12 22:46:52 +0300

    edit docs

--- models/processing.py
+++ models/processing.py
@@ -28,28 +28,81 @@ def get_table_description(filename: str,
     return table_description
 
 
-class DataProcess:
+class ProcessEDA:
+    """
+    Class for Exploratory Data Analysis.
+
+    Parameters:
+    ----------
+    data : pd.DataFrame
+        DataFrame for analysis. Any format, any columns.
+
+    table_description : pd.DataFrame, default=None
+        DataFrame with explanation of each column in main data.
+        columns = {"Table", "Row", "Description", "Special"}
+
+    target_column : str, default=None
+        Name of target column in data.
+
+    id_column : str, default=None
+        Name of column with row id.
+
+    date_column : str, default=None
+        Name of column with dates.
+
+    Attributes:
+    ----------
+    data : pd.DataFrame, from constructor.
+
+    table_description :  pd.DataFrame, from constructor.
+
+    target_column : str, from constructor.
+
+    id_column : str, from constructor.
+
+    date_column : str, from constructor.
+
+    column_types : dict,
+        column_types.keys() = dict_keys(['Continuous', 'Binary', 'Nominal', 'Unknown']).
+    """
 
     def __init__(self,
                  data: pd.DataFrame,
-                 table_description=None):
+                 table_description=None,
+                 target_column=None,
+                 id_column=None,
+                 date_column=None):
+
         self.data = data
         self.table_description = table_description
-        self.column_types = None
+        self.target_column = target_column
+        self.id_column = id_column
+        self.date_column = date_column
 
-    def get_column_types(self,
-                         exclude_col=None,
-                         include_col=None):
+        self.column_types = self._get_column_types(exclude_col=[target_column, id_column])
+
+    def _get_column_types(self,
+                          exclude_col=None,
+                          include_col=None):
         """
         Finds type of data for each column.
-            exclude_col: list of str, which columns exclude. Default None, exclude 0 columns.
-            include_col: list of str, which columns include. Default None, include all columns.
+
+        Parameters:
+        ----------
+        exclude_col : list of str, default=None.
+            Which columns excluded. If None -> exclude 0 columns.
+
+        include_col : list of str, default=None.
+            Which columns included. If None -> include all columns.
+
         Returns:
-            column_types: dict with columns of each data type:
-                             'Continuous' -- key of decimal columns (Numerical);
-                             'Binary' -- key of binary columns, Yes/No or 1/0 (Categorical);
-                             'Nominal' -- key of nominal columns, string (Categorical);
-                             'Unknown' -- key of columns that could be both (Numerical,Categorical).
+        --------
+        column_types: dict,
+            Columns associated with dtypes in the form "{dtype: [columns]}"
+            'Continuous' -- key of decimal columns (Numerical);
+            'Binary' -- key of binary columns, Yes/No or 1/0 (Categorical);
+            'Nominal' -- key of nominal columns, string (Categorical);
+            'Unknown' -- key of columns that could be both (Numerical,Categorical).
         """
         data = self.data
         column_types = dict()
@@ -70,21 +123,27 @@ class DataProcess:
         unknown_columns = []
 
         for column in all_columns:
-            dtype = data.dtypes[column]
+            col_dtype = data.dtypes[column]
             n_unique = len(data[column].unique())
 
-            if dtype == 'object':
+            if col_dtype == 'datetime64':
+                continue
+            elif col_dtype == 'bool':
+                binary_columns.append(column)
+            elif col_dtype == 'object':
                 if n_unique == 2:
                     binary_columns.append(column)
                 else:
                     nominal_columns.append(column)
-            else:
+            elif col_dtype in ['float64', 'int64']:
                 if n_unique == 2:
                     binary_columns.append(column)
                 elif 3 <= n_unique <= 15:
                     unknown_columns.append(column)
                 else:
                     continuous_columns.append(column)
+            else:
+                unknown_columns.append(column)
 
         column_types['Continuous'] = continuous_columns
         column_types['Binary'] = binary_columns
@@ -100,22 +159,27 @@ class DataProcess:
         Show information of particular data type.
         Format of display - Data Type, Column Name, Description(Optional), Distribution.
 
-        Arguments:
-            display_type: str, one of ('Continuous', 'Binary', 'Nominal', 'Unknown').
-
+        Parameters:
+        ----------
+        display_type: str,
+            What type of data to display. One of {'Continuous', 'Binary', 'Nominal', 'Unknown'}.
         """
         data = self.data
         table_description = self.table_description
 
         if self.column_types is None:
-            self.get_column_types()
+            self._get_column_types()
         column_types = self.column_types
 
         display_columns = column_types[display_type]
         print(display_type, ' Data')
         for column in display_columns:
-            col_description = table_description[column].values[0]
             col_distribution = data[column].value_counts(normalize=True)
+
+            if table_description is not None:
+                col_description = table_description[column].values[0]
+            else:
+                col_description = ''
 
             print("\n==========\n",
                   column, ' - ', col_description,
@@ -128,7 +192,9 @@ application_train_description = get_table_description(filename='../data/HomeCred
                                                       table_name='application_{train|test}.csv')
 
 
-process_data = DataProcess(data=application_train,
-                           table_description=application_train_description)
+process_data = ProcessEDA(data=application_train,
+                          table_description=application_train_description,
+                          target_column='TARGET',
+                          id_column='SK_ID_CURR')
 
-print(process_data.get_column_types())
+print(process_data.column_types)
